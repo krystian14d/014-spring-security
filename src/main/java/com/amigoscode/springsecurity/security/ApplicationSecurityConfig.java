a5fbcd2799602ff2a199829eb4dd8a9e9597e4dd
Krystian <krystian14d@gmail.com>
2022-04-29 09:34:53 +0200

    password encoder and InMemoryUserDetailsManager

--- src/main/java/com/amigoscode/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,23 @@
 package com.amigoscode.springsecurity.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +29,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
