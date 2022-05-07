f7636b5af837101d26dc0ccb0bcb8901380160a2
Krystian <krystian14d@gmail.com>
2022-05-07 19:13:21 +0200

    JWT username and password auth filter

---
+++ src/main/java/com/amigoscode/springsecurity/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.amigoscode.springsecurity.jwt;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UsernameAndPasswordAuthenticationRequest {
+
+    private String username;
+    private String password;
+
+
+}
