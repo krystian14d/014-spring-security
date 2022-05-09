77993847a6ea73261f1e5ef516ccaa5df36c8b16
Krystian <krystian14d@gmail.com>
2022-05-09 10:47:15 +0200

    adnotation update

--- src/main/java/com/amigoscode/springsecurity/jwt/JwtConfig.java
+++ src/main/java/com/amigoscode/springsecurity/jwt/JwtConfig.java
@@ -1,18 +1,16 @@
 package com.amigoscode.springsecurity.jwt;
 
 import com.google.common.net.HttpHeaders;
-import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import javax.crypto.SecretKey;
+import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "application.jwt")
 public class JwtConfig {
 
@@ -20,7 +18,7 @@ public class JwtConfig {
     private String tokenPrefix;
     private Integer tokenExpirationAfterDays;
 
-    public String getAuthorizationHeader(){
+    public String getAuthorizationHeader() {
         return HttpHeaders.AUTHORIZATION;
     }
 }
